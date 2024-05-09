9bfdc7c094eba039acb068e72cb4623139a6c5ee
JoSeungHwan <swcc321@naver.com>
2024-05-09 17:41:13 +0900

    수정후 재업

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+import { Store } from '../core/jo';
+
+export default new Store({
+  photo:
+    '	https://avatars.githubusercontent.com/u/111415219?u=aff17905a0f1263d4f21cc37af3b3dece7e6c2ba&v=4',
+  name: 'Jo / JoSeungHwan',
+  email: 'swcc321@naver.com',
+  blog: 'https://heropy.blog',
+  github: 'https://github.com/JoSeungHwan',
+  repository: 'https://github.com/JoSeungHwan/-vanillajs-movie-app',
+});
